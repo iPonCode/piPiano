00809af22daf112a3722f631cb8c8bdcfbf88479
superw <sw@swmbp13.local>
2018-12-07 15:56:05 +0100

    Connecting multiple buttons with the same @IBAction. We reference each button in code using tags defined in Interface Builder: Any graphic interface item was a parameter called tag (not only the UIButton) and this tag is a unique identifier that we can use in code. We named the tags just equal than the index of an array, something like the button1’s tag is 1, buttons2’s tag es 2, button3’s tag es 3.. This is because we have created an array of String that content the name of the mp3 file of each sound, so we can obtain the sound simply retrieving the array's value in the key pressed position - 1. Tags: @IBAction, tag, TouchUpInside, connect

--- piPiano/Controllers/ViewController.swift
+++ piPiano/Controllers/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let soundsNames = ["c1","c1s","d1","d1s","e1","f1","f1s","g1","g1s","a1","a1s","b1","c2"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    //al pulsar el botón (la tecla del piano) y levantar el dedo se ejecutará el código de esta función.
+    //para que funcione para todos y cada uno de los botones, una vez hayamos arrastrado con ctrl pulsado el primer botón para dejarlo configurado creando la Action para ese botón, entonces vamos arrastando también con ctrl pulsado cada uno de los botones que queramos (son 13 en total) hasta  esta misma función soltándolos en el código dijándonos bien en que aparezca un mensaje que indica "Connect Action" y NO el de "Insert Action, Outlet.."
+    //para comprobar que están hechas las 13 conexiones correspondientes a cada botón o tecla del piano, posicionamos el cursor del ratón encima del número de línea del código correspondiente con la declaración de la función "@IBAction func" (en lugar del núero de línea aparece una bolita con un signo + en el interior) y en el StoryBoard se deben marcar todos los elementos conectados.
+    @IBAction func TouchUpInsidePlay(_ sender: UIButton) {
+        let tagKeyPressed = sender.tag
+        let fileName = soundsNames[tagKeyPressed - 1]
+        print("-> Se ha SOLTADO la tecla >>> \(tagKeyPressed) <<< >>> \(fileName)")
+    }
+    
+    @IBAction func TouchDownPlay(_ sender: UIButton) {
+        /*let tagKeyPressed = sender.tag
+        let fileName = soundsNames[tagKeyPressed - 1]
+        print("-> Se ha PULSADO la tecla >>> \(tagKeyPressed) <<< >>> \(fileName)")*/
+    }
 }
 
